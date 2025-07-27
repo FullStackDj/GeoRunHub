c348379868cb16d44a9e746aeec655258fcc0ec3
FullStackDj <full.stackdj@gmail.com>
2025-07-27 09:25:50 +0300

    Implemented data retrieval and display for content in view

---
+++ GeoRunHub/Controllers/ClubController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GeoRunHub.Data;
+using GeoRunHub.Models;
+
+namespace GeoRunHub.Controllers;
+
+public class ClubController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ClubController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        List<Club> clubs = _context.Clubs.ToList();
+        return View(clubs);
+    }
+}
