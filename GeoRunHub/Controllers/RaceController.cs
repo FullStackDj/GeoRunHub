35eb07a8d16f0a3095cc734a617d32961fb60eb9
FullStackDj <full.stackdj@gmail.com>
2025-07-27 11:35:29 +0300

    Added data fetching and UI rendering for list of items

---
+++ GeoRunHub/Controllers/RaceController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GeoRunHub.Data;
+using GeoRunHub.Models;
+
+namespace GeoRunHub.Controllers;
+
+public class RaceController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public RaceController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        List<Race> races = _context.Races.ToList();
+        return View(races);
+    }
+}
