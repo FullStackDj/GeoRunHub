1995b31da06a88b33b7ccf469521df5239ba0031
FullStackDj <full.stackdj@gmail.com>
2025-07-29 10:26:33 +0300

    Added repository pattern with async methods and address include

---
+++ GeoRunHub/Interfaces/IClubRepository.cs
@@ -0,0 +1,14 @@
+﻿using GeoRunHub.Models;
+
+namespace GeoRunHub.Interfaces;
+
+public interface IClubRepository
+{
+    Task<IEnumerable<Club>> GetAll();
+    Task<Club> GetByIdAsync(int id);
+    Task<IEnumerable<Club>> GetClubByCity(string city);
+    bool Add(Club club);
+    bool Update(Club club);
+    bool Delete(Club club);
+    bool Save();
+}
