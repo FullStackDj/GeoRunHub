0db9c348db082654151a90cbb87f02120667719f
FullStackDj <full.stackdj@gmail.com>
2025-08-09 17:19:50 +0300

    Added account login controller and view model with authentication logic

---
+++ GeoRunHub/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GeoRunHub.ViewModels;
+
+public class LoginViewModel
+{
+    [Display(Name = "Email Address")]
+    [Required(ErrorMessage = "Enter your email address")]
+    public string EmailAddress { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
