3e91653a5b991e4b256e92f7a23e63b475f2a13d
FullStackDj <full.stackdj@gmail.com>
2025-08-10 18:07:09 +0300

    Implemented account registration controller, view model and logout functionality

--- GeoRunHub/Controllers/AccountController.cs
+++ GeoRunHub/Controllers/AccountController.cs
@@ -57,4 +57,49 @@ public class AccountController : Controller
         TempData["Error"] = "Invalid Email or Password";
         return View(loginViewModel);
     }
+
+    public IActionResult Register()
+    {
+        var response = new RegisterViewModel();
+        return View(response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(registerViewModel);
+        }
+
+        var user = await _userManager.FindByEmailAsync(registerViewModel.EmailAddress);
+
+        if (user != null)
+        {
+            TempData["Error"] = "Email already exists";
+            return View(registerViewModel);
+        }
+
+        var newUser = new AppUser()
+        {
+            Email = registerViewModel.EmailAddress,
+            UserName = registerViewModel.EmailAddress,
+        };
+
+        var newUserResponse = await _userManager.CreateAsync(newUser, registerViewModel.Password);
+
+        if (newUserResponse.Succeeded)
+        {
+            await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+        }
+
+        return RedirectToAction("Index", "Race");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Race");
+    }
 }
---
+++ GeoRunHub/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GeoRunHub.ViewModels;
+
+public class RegisterViewModel
+{
+    [Display(Name = "Email address")]
+    [Required(ErrorMessage = "Enter your email address")]
+    public string EmailAddress { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Display(Name = "Confirm password")]
+    [Required(ErrorMessage = "Confirm your password")]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; }
+}
