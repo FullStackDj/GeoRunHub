4c29efdac03450cc4515a6cca4d433d798e7518c
FullStackDj <full.stackdj@gmail.com>
2025-08-13 12:24:02 +0300

    Created user list and detail view models

---
+++ GeoRunHub/ViewModels/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GeoRunHub.ViewModels;
+
+public class UserViewModel
+{
+    public string Id { get; set; }
+    public string UserName { get; set; }
+    public int? Pace { get; set; }
+    public int? Mileage { get; set; }
+}
