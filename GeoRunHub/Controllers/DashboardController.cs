937876a369294c66f0d19ed6535037b1f581cde1
FullStackDj <full.stackdj@gmail.com>
2025-08-15 13:00:53 +0300

    Implemented dashboard actions for editing user profile

--- GeoRunHub/Controllers/DashboardController.cs
+++ GeoRunHub/Controllers/DashboardController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Mvc;
 using GeoRunHub.Interfaces;
+using GeoRunHub.Models;
 using GeoRunHub.ViewModels;
 
 namespace GeoRunHub.Controllers;
@@ -7,10 +9,25 @@ namespace GeoRunHub.Controllers;
 public class DashboardController : Controller
 {
     private readonly IDashboardRepository _dashboardRepository;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly IPhotoService _photoService;
 
-    public DashboardController(IDashboardRepository dashboardRepository)
+    public DashboardController(IDashboardRepository dashboardRepository, IHttpContextAccessor httpContextAccessor,
+        IPhotoService photoService)
     {
         _dashboardRepository = dashboardRepository;
+        _httpContextAccessor = httpContextAccessor;
+        _photoService = photoService;
+    }
+
+    private void MapUserEdit(AppUser user, EditUserDashboardViewModel editVM, ImageUploadResult photoResult)
+    {
+        user.Id = editVM.Id;
+        user.Pace = editVM.Pace;
+        user.Mileage = editVM.Mileage;
+        user.ProfileImageUrl = photoResult.Url.ToString();
+        user.City = editVM.City;
+        user.State = editVM.State;
     }
 
     public async Task<IActionResult> Index()
@@ -24,5 +41,70 @@ public class DashboardController : Controller
         };
 
         return View(dashboardViewModel);
+    }
+
+    public async Task<IActionResult> EditUserProfile(int id)
+    {
+        var curUserId = _httpContextAccessor.HttpContext.User.GetUserId();
+        var user = await _dashboardRepository.GetUserById(curUserId);
+
+        if (user == null)
+        {
+            return View("Error");
+        }
+
+        var editUserViewModel = new EditUserDashboardViewModel()
+        {
+            Id = curUserId,
+            Pace = user.Pace,
+            Mileage = user.Mileage,
+            ProfileImageUrl = user.ProfileImageUrl,
+            City = user.City,
+            State = user.State
+        };
+
+        return View(editUserViewModel);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> EditUserProfile(EditUserDashboardViewModel editViewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            ModelState.AddModelError("", "Invalid data");
+            return View("EditUserProfile", editViewModel);
+        }
+
+        var user = await _dashboardRepository.GetByIdNoTracking(editViewModel.Id);
+
+        ImageUploadResult? photoResult;
+        if (string.IsNullOrEmpty(user.ProfileImageUrl))
+        {
+            photoResult = await _photoService.AddPhotoAsync(editViewModel.Image);
+
+            MapUserEdit(user, editViewModel, photoResult);
+
+            _dashboardRepository.Update(user);
+
+            return RedirectToAction("Index");
+        }
+
+        try
+        {
+            await _photoService.DeletePhotoAsync(user.ProfileImageUrl);
+        }
+        catch (Exception ex)
+        {
+            ModelState.AddModelError("", ex.Message);
+            return View(editViewModel);
+        }
+
+        photoResult = await _photoService.AddPhotoAsync(editViewModel.Image);
+
+        MapUserEdit(user, editViewModel, photoResult);
+
+        _dashboardRepository.Update(user);
+
+        return RedirectToAction("Index");
     }
 }
