90a5748f0055996dcc0d9033c9f50a64dc7fc4ab
FullStackDj <full.stackdj@gmail.com>
2025-08-17 18:24:06 +0300

    Created home view displaying nearby running clubs

---
+++ GeoRunHub/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using GeoRunHub.Models;
+
+namespace GeoRunHub.ViewModels;
+
+public class HomeViewModel
+{
+    public IEnumerable<Club> Clubs { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+}
